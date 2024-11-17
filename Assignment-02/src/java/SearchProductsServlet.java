7a223fdbe3935b258ba8314b75f841337dfa26a8
AshmitaRavindra <220701031@rajalakshmi.edu.in>
2024-11-17 22:33:44 +0530

    Update SearchProductsServlet.java

--- Assignment-02/src/java/SearchProductsServlet.java
+++ Assignment-02/src/java/SearchProductsServlet.java
@@ -14,10 +14,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author AFRIN
- */
 
 public class SearchProductsServlet extends HttpServlet {
 
